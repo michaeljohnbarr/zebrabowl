577430b1f7bd66fa27c2b506a5cdf04cea4dbc60
Scott Crespo <screspo@highcornergroup.com>
2014-10-13 23:33:45 -0400

    include url statements to root urlconf

--- zebrabowl/urls.py
+++ zebrabowl/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,4 +8,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/',include('apps.accounts.urls')),
+    url(r'^scorecard/',include('apps.accounts.urls')),
 )
