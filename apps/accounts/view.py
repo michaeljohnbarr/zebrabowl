2df75c9f8e67d296ee852c807dca764220fee958
Scott Crespo <screspo@highcornergroup.com>
2014-10-14 00:10:28 -0400

    deleted accounts

--- apps/accounts/view.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect
-from django.core.urlresolvers import reverse
-from django.contrib.auth.models import User
-from .forms import *
-
-def new_user(request):
-	"""Foo"""
-	
