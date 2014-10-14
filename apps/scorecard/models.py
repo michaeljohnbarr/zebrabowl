f8165693e5dc2a18d1670b30d295be8719f80a4c
Scott Crespo <screspo@highcornergroup.com>
2014-10-13 22:11:40 -0400

    created rank field on scorecard

--- apps/scorecard/models.py
+++ apps/scorecard/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime
 
 class Game(models.Model):
-    date_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now_add=True)    
+    
     class Meta:
-        db_table = 'game'
-        
+        db_table = 'game'    
     
 class ScoreCard(models.Model):
-    user = models.OneToOneField(User)
-    game = models.OneToOneField(Game)
-    score = models.PositiveSmallIntegerField(default = 0, )
+    user = models.ManyToManyField(User)
+    game = models.ForeignKey(Game)
+    total_score = models.PositiveSmallIntegerField(default = 0, )
+    rank = models.PositiveSmallIntegerField(blank=True )
     
     class Meta:
         db_table = 'score_card'
