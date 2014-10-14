d635f9158683d31eb2ca9a372aada8e058fe4dc2
Scott Crespo <screspo@highcornergroup.com>
2014-10-13 21:24:29 -0400

    create frame and scorecard models

--- apps/scorecard/models.py
+++ apps/scorecard/models.py
@@ -1,3 +1,21 @@
-from django.db.models import Model
+from django.db import models
 from django.contrib.auth.models import User
 
+class ScoreCard(models.Model):
+    user = models.OneToOneField(User)
+    score = models.PositiveSmallIntegerField(default = 0, )
+    
+    class Meta:
+        db_table = 'score_card'
+
+class Frame(models.Model):
+    score_card = models.ForeignKey(ScoreCard)
+    number = models.PositiveSmallIntegerField(min_value = 1, max_value = 10)
+    down_pins1 = models.PositiveSmallIntegerField(min_value=0, max_value = 10)
+    down_pins2 = models.PositiveSmallIntegerField(min_value=0, max_value = 10)    
+    is_strike = models.BooleanField(default=False)
+    is_spare = models.BooleanField(default = False)
+    score = models.PositiveSmallIntegerField(default=0)
+    class Meta:
+        db_table = 'frame'
+        
