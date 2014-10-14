25a36f27d3749d0e0fbc9e532b87225e7c6d95a8
Scott Crespo <screspo@highcornergroup.com>
2014-10-14 13:09:41 -0400

    crispy form renders!

--- apps/scorecard/forms.py
+++ apps/scorecard/forms.py
@@ -4,10 +4,15 @@ from crispy_forms.layout import Submit
 from .models import *
 
 class NewScoreCardForm(forms.ModelForm):
+            
+    def __init__(self, *args, **kwargs):
+        super(NewScoreCardForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-scorecard-from'
+        self.helper.form_class = 'horizontal-form'
+        self.helper.form_action = ''
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Submit'))
     class Meta:
         model = ScoreCard
         fields = ['player_name',]
-        
-    
-        
-    
