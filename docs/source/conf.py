65af75ba6b2c33d94cbdd5db50f2af6c35ab1783
Scott Crespo <screspo@highcornergroup.com>
2014-10-20 11:59:38 -0400

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,11 +14,11 @@
 
 import sys
 import os
-from settings.base import *
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
 
 sys.path.append(
                 os.path.abspath(os.path.join(os.path.dirname(__file__),'../../'))
