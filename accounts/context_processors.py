3bcdd82f3faf0f74835a1b9d987f317808e83295
Scott Crespo <screspo@highcornergroup.com>
2014-10-28 15:30:43 -0400

    error handling of accounts context proc

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 
 def profile(request):
     
-    profile = UserProfile.objects.get(user=request.user)
-    
+    try:
+        profile = UserProfile.objects.get(user=request.user)
+    except TypeError:
+        profile = None
         
     return {'PROFILE':profile}
