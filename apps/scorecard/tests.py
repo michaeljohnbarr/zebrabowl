25b2cdb36641c217ba5f1aa5cf96f1ecfe7fa521
Scott Crespo <screspo@highcornergroup.com>
2014-11-02 20:54:48 -0500

    working on view tests

--- apps/scorecard/tests.py
+++ apps/scorecard/tests.py
@@ -1,6 +1,22 @@
 from django.test import TestCase
-from .models import *
+from django.test.client import Client
+from django.core.urlresolvers import reverse
 
-class GameTestCase(TestCase):
-    def set_up(self):
-        Game.objects.create(game_hash="myfakehash")
+class ViewsTestCase(TestCase):
+    
+    client = Client()
+    
+    def test_signin(self):
+    
+        response = self.client.post(reverse('userena_signin'),
+                                    {'identification': 'sccrespo@gmail.com','password': 'a'})
+        
+        self.assertRedirects(response, reverse('userena_profile_detail',
+                                               kwargs={'username': '0ef86'}))
+        
+        
+    def test_new_game(self):
+        
+        response = self.client.get(reverse('newgame'))
+        
+        self.assertEqual(response.status_code, 200)
