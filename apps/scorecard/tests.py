a39fc526cc95849527ad63b81e9450166327af6a
Scott Crespo <screspo@highcornergroup.com>
2014-11-03 15:43:18 -0500

    added perfect strike and spare games for testing

--- apps/scorecard/tests.py
+++ apps/scorecard/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 from django.test.client import Client
 from django.core.urlresolvers import reverse
+from .models import *
 
-class ViewsTestCase(TestCase):
-    
+class TestCaseHelper(TestCase):
     fixtures = ['accounts_views_testdata.json',
                 'auth_views_testdata.json',
                 'scorecard_views_testdata.json',
@@ -13,12 +13,26 @@ class ViewsTestCase(TestCase):
     identification = 'sccrespo@gmail.com'
     password = 'a'
     
+    game_board_path = reverse('gameboard', kwargs={'username':username})
+    
     def signin(self):
         
         response = self.client.post(reverse('userena_signin'),
             {'identification': self.identification,'password': self.password})
     
         return response
+    
+    def newgame(self):
+        
+        self.client.get(reverse('newgame'))
+        
+    def add_players(self):
+                        
+        path = reverse('addplayers', kwargs={'username':self.username})                
+        self.client.post(path, data= {'player_name':'Socrates'})
+        
+
+class ViewsTestCase(TestCaseHelper):
     
     def test_signin(self):
     
@@ -52,7 +66,7 @@ class ViewsTestCase(TestCase):
         """Tests GET and POST requests for the game_board view"""
         
         self.signin()
-        path = reverse('gameboard', kwargs={'username':self.username})
+        path = self.game_board_path
         # Get and Post Requests
         response_get = self.client.get(path)
         response_post = self.client.post(path, data={'down_pins1':'10',
@@ -65,4 +79,37 @@ class ViewsTestCase(TestCase):
         # Check on Session Data
         self.assertEqual(self.client.session['player_num'], 1)
         self.assertEqual(self.client.session['frame_num'], 2)
-    
+        
+class GameTestCase(TestCaseHelper):
+
+    def test_perfect_game(self):
+        """Bowl a Perfect Game and Check for score == 300"""
+        self.signin()
+        self.newgame()
+        self.add_players()
+        
+        # Bowl 12 frames (instead of 10) because there will be bonus frames
+        # created to account for consecutive strikes
+        i=0
+        while i < 12: 
+            self.client.post(self.game_board_path, data={'down_pins1':'10',
+                                                         'down_pins2':'0',})
+            i+=1
+        
+        scorecard = ScoreCard.objects.last()
+        self.assertEqual(scorecard.total_score, 300)
+        
+    def test_spare_game(self):
+        """Bowl a game of straight Spares and check that score == 100"""
+        self.signin()
+        self.newgame()
+        self.add_players()
+        
+        i=0
+        while i < 11: 
+            self.client.post(self.game_board_path, data={'down_pins1':'0',
+                                                         'down_pins2':'10',})
+            i+=1
+            
+        scorecard = ScoreCard.objects.last()
+        self.assertEqual(scorecard.total_score, 100)
