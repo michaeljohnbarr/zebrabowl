fb213a270839b2267fb8cfa4c952ee4b5e564982
Scott Crespo <screspo@highcornergroup.com>
2014-11-03 09:33:48 -0500

    add players test

--- apps/scorecard/tests.py
+++ apps/scorecard/tests.py
@@ -4,19 +4,44 @@ from django.core.urlresolvers import reverse
 
 class ViewsTestCase(TestCase):
     
-    client = Client()
+    fixtures = ['accounts_views_testdata.json',
+                'auth_views_testdata.json',
+                'scorecard_views_testdata.json',
+                'userena_views_testdata.json',]
+    
+    username = '0ef86'
+    identification = 'sccrespo@gmail.com'
+    password = 'a'
+    
+    def signin(self):
+        
+        response = self.client.post(reverse('userena_signin'),
+            {'identification': self.identification,'password': self.password})
+    
+        return response
     
     def test_signin(self):
     
-        response = self.client.post(reverse('userena_signin'),
-                                    {'identification': 'sccrespo@gmail.com','password': 'a'})
+        response = self.signin()
         
         self.assertRedirects(response, reverse('userena_profile_detail',
-                                               kwargs={'username': '0ef86'}))
+                                               kwargs={'username': self.username}))
         
         
     def test_new_game(self):
         
+        self.signin()
+        
         response = self.client.get(reverse('newgame'))
         
         self.assertEqual(response.status_code, 200)
+        
+    def test_add_players(self):
+        self.signin()                
+        path = (reverse('addplayers', kwargs={'username':self.username}))        
+        # Get and Post Requests
+        response_get = self.client.get(path)        
+        response_post = self.client.post(path, data= {'player_name':'Socrates'})
+        # Assertions
+        self.assertEqual(response_get.status_code, 200)        
+        self.assertEqual(response_post.status_code, 302)
