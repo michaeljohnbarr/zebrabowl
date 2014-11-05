89289a61e73844c58609e4a05084ae507618734e
Scott Crespo <screspo@highcornergroup.com>
2014-11-04 22:23:18 -0500

    updated production settings

--- manage.py
+++ manage.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
     else:
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.base")
-    
-    sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+        
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
