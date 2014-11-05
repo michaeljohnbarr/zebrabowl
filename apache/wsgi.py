c1ce77592b8a88635d95421d0ffdd89b636fe450
Scott Crespo <screspo@highcornergroup.com>
2014-11-04 22:14:23 -0500

    updated production settings

--- apache/wsgi.py
+++ apache/wsgi.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 import socket
-
+import sys
 
 hostname = socket.gethostname()
     
 if hostname == "highcorner1":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
+    sys.path.append('/home/scott/zebrabowl')
 else:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.base")
 
